b9ad544893fd8dd465bce7a9c8f2c632609066a2
stoutheo06 <stoutheo@gmail.com>
2019-01-31 10:18:14 +0000

    Adding clean python to c auto generated code and loading the auto generated compiled c code from c++

---
+++ py2cpp/cpp/libUse.cpp
@@ -0,0 +1,35 @@
+ // C++ (and CasADi)
+#include <casadi/casadi.hpp>
+using namespace std;
+
+
+void usage_cplusplus(){
+  cout << "---" << endl;
+  cout << "Usage from CasADi C++:" << endl;
+  cout << endl;
+
+  // Use CasADi's "external" to load the compiled function
+  casadi::Function f = casadi::external("f", "../../lib/libExample.so");
+
+  // Use like any other CasADi function
+
+  // example values 1st ex
+  vector<double> x = {1, 2, 3, 4};
+  vector<casadi::DM> arg = {reshape(casadi::DM(x), 4, 1), 3};
+
+  // example values 2nd ex
+  // vector<DM> arg = {DM(0), 3};
+
+  vector<casadi::DM> res = f(arg);
+
+  cout << "result (0): " << res << endl;
+
+}
+
+int main(){
+
+  // Usage from C++
+  usage_cplusplus();
+
+  return 0;
+}
---
+++ py2cpp/libGen.py
@@ -0,0 +1,43 @@
+from sys import path
+import numpy as np
+
+# temp intallation folder of CasADi (pre-compiled version)
+path.append(r"/home/theo/software/tools/CasADi/test/casadi-linux-py27-58aa427")
+
+from casadi import *
+
+from os import system
+
+# source : https://gist.github.com/jgillis/41abbe1c9f2f2736024bb1ecb33ee51c
+
+x=SX.sym("x",4,1)
+y=SX.sym("y")
+
+z = vertcat(sin(x[0]*y),cos(x[1]*sqrt(y)-x[2]),x[3])
+
+f = Function('f',[x,y],[z])
+
+#  make the generator for storing all info as .so
+cg = CodeGenerator('libExample')
+cg.add(f)
+cg.add(f.forward(1))
+cg.add(f.reverse(1))
+cg.add(f.reverse(1).forward(1))
+# generate c file
+cg.generate()
+
+#  compile it
+system("gcc -fPIC -shared -O3 libExample.c -o libExample.so")
+
+# if we wanted to load it in python
+# f_ext = external("f","./code.so")
+
+# to move files to correct location
+import shutil
+
+# move it to the c_code folder
+shutil.move("libExample.c", "c_code/libExample.c")
+
+# move it to the lib folder
+shutil.move("libExample.so", "lib/libExample.so")
+#
